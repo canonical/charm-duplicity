feec287d1e03636a80f540c3dd80cfd1ec921abd
Ryan A Farrell <ryan.farrell@canonical.com>
2019-11-01 13:22:02 +0000

    Initial commit.

---
+++ tests/unit/test_lib.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+class TestLib():
+    def test_pytest(self):
+        assert True
+
+    def test_duplicity(self, duplicity):
+        ''' See if the helper fixture works to load charm configs '''
+        assert isinstance(duplicity.charm_config, dict)
+
+    # Include tests for functions in lib_duplicity
