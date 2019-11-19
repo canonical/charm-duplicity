126593c8c5c3b7497bd94cb69a68c8b3a9743734
Ryan A Farrell <ryan.farrell@canonical.com>
2019-11-19 18:50:53 +0000

    Unit tests and new config options
    * Added Unit tests for the _backup_url assembly function and resolved errors
    * Changed "show_history" action to "verify"
    * Cleaned up config descriptions
    * Added new config options for encyption and cron frequency
    * Added checks for config data validity in reactive code
    * Added placeholder functions to lib for duplicity's actions

--- lib/lib_duplicity.py
+++ lib/lib_duplicity.py
@@ -6,21 +6,6 @@ class DuplicityHelper():
     def __init__(self):
         self.charm_config = hookenv.config()
 
-    def show_history(self):
-        """ Show the backup history by date and filesize """
-        #TODO
-        return
-
-    def do_backup(self):
-        """ Execute the backup script as configured by the charm """
-        print(self.charm_config)
-        print(self.charm_config.get("backend"))
-
-        cmd = ["duplicity", "--version"]
-        # FIXME - To implement...
-        subprocess.check_call(cmd)
-        return
-
     def _backup_url(self):
         """
         Helper function to assemble the backup url into a format accepted
@@ -28,35 +13,30 @@ class DuplicityHelper():
         defined in the charm config
         """
         backend = self.charm_config.get("backend").lower()
+        # start building the url
+        url = "{}://".format(backend)
+
         if backend in ["rsync", "scp", "ssh"]:
             # These all require SSH Type Authentication and will attempt to use
             # the provided remote host credentials
             user = self.charm_config.get("remote_user")
-            password = self.charm.config.get("remote_password")
-
-            # start building the url
-            url = "{}://".format(backend)
+            password = self.charm_config.get("remote_password")
             if user:
                 if password:
-                    url +=  "{}:{}@".format(user, password)
+                    url += "{}:{}@".format(user, password)
                 else:
                     url += "{}@".format(user)
             url += self.charm_config.get("remote_backup_url")
+
             return url
-            # TODO - UNIT TESTS!
-            # rsync://user[:password]@other.host[:port]::/module/some_dir
-            # rsync://user[:password]@other.host[:port]/relative_path
-            # rsync://user[:password]@other.host[:port]//absolute_path
-            # scp://user[:password]@other.host[:port]/some_dir
-            # ssh://user[:password]@other.host[:port]/some_dir
+
         elif backend in ["ftp", "ftps"]:
             # FTP requires remote credentials to be set, IIRC...
             # TODO - impl. later
             raise NotImplementedError
         elif backend == "s3":
-            # This will require AWS IMA user keys, TODO: check for those later
             # The url should already be well formed
-            return self.charm_config.get("remote_backup_url")
+            return url + self.charm_config.get("remote_backup_url")
         elif backend == "local":
             #local file, ensure it looks like a proper local url ""
             url = self.charm_config.get("remote_backup_url")
@@ -64,14 +44,56 @@ class DuplicityHelper():
                 return url
             else:
                 return "file://{}".format(url)
-        elif backend != "":
-            # invalid backend set - we should log a warning then not do the
-            # requested action later
-            # TODO - figure out logging
-            # does this belong in /var/log/juju/unit-duplicity?
-            # or /var/log/duplicity
-            pass
 
         return None
 
+    def do_backup(self):
+        """ Execute the backup script as configured by the charm """
+        print(self.charm_config)
+        print(self.charm_config.get("backend"))
+        print(self._backup_url())
 
+        cmd = ["duplicity", "--version"]
+        # FIXME - To implement...
+        subprocess.check_call(cmd)
+        return
+
+    def cleanup(self):
+        #TODO
+        # duplicity cleanup <target_url>
+        pass
+
+    def verify(self):
+        #TODO
+        # duplicity verify <target_url> <source_dir>
+        pass
+
+    def collection_status(self):
+        #TODO
+        # duplicity collection-status <target_url>
+        pass
+
+    def list_current_files(self):
+        #TODO
+        # duplicity list-current-files <target_url>
+        pass
+
+    def restore(self):
+        #TODO
+        # duplicity restore <source_url> <target_dir>
+        pass
+
+    def remove_older_than(self):
+        #TODO
+        # duplicity remove-older-than time [options] target_url
+        pass
+
+    def remove_all_but_n_full(self):
+        #TODO
+        # duplicity remove-all-but-n-full <count> <target_url>
+        pass
+
+    def remove_all_inc_of_but_n_full(self):
+        #TODO
+        # duplicity remove-all-inc-of-but-n-full <count> <target_url>
+        pass
--- reactive/duplicity.py
+++ reactive/duplicity.py
@@ -1,7 +1,9 @@
 from lib_duplicity import DuplicityHelper
 from charmhelpers.core import hookenv, host
-from charms.reactive import set_flag, when_not
+from charms.reactive import set_flag, clear_flag, when_not, when
 from charmhelpers import fetch
+
+import os
 
 helper = DuplicityHelper()
 
@@ -27,4 +29,84 @@ def install_duplicity():
 
     hookenv.status_set('active', '')
     set_flag('duplicity.installed')
+
+
+@when('config.set.backend')
+def validate_backend():
+    """
+    Validates that the config value for 'backend' is something that duplicity
+    can use (see config description for backend for the accepted types). For S3
+    only, check that the AWS IMA credentials are also set.
+    """
+    backend = hookenv.config().get("backend").lower()
+    if backend == "":
+        hookenv.status_set('blocked', 'The backup backend is not defined')
+        return False
+    elif backend not in ["s3", "ssh", "scp", "ftp", "rsync", "local"]:
+        hookenv.status_set('blocked',
+                           'Unrecognized backend "{}"'.format(backend))
+        return False
+    elif backend == "s3":
+        # make sure 'aws_access_key_id' and 'aws_secret_access_key' exist
+        if not hookenv.config().get("aws_access_key_id") or \
+                not hookenv.config().get("aws_secret_access_key"):
+            hookenv.status_set('blocked', 'S3 backups require \
+                    "aws_access_key_id" and "aws_secret_access_key" to be set')
+            return False
+    return True
+
+
+@when('config.set.aux_backup_directory')
+def create_aux_backup_directory():
+    aux_backup_dir = hookenv.config().get("aux_backup_directory")
+    if aux_backup_dir:
+        # if the data is not ok to make a directory path then let juju catch it
+        os.mkdir(aux_backup_dir)
+        hookenv.log(
+            "Creating auxiliary backup directory: {}".format(aux_backup_dir))
+
+@when('config.set.backup_frequency')
+def validate_cron_frequency():
+    cron_frequency = hookenv.config().get("backup_frequency").lower()
+    if cron_frequency not in ["daily", "weekly", "monthly", "manual", "auto"]:
+        hookenv.status_set('blocked',
+                           'Unknown value "{}" for cron frequency'.format(
+                               cron_frequency)
+                           )
+        return False
+    return True
+
+@when('config.set.encryption_passphrase')
+@when('config.set.gpg_public_key')
+@when('config.set.disable_encryption')
+def validate_encryption_method():
+    """
+    Function to check that a viable encryption method is configured.
+    """
+    passphrase = hookenv.config().get("encryption_passphrase")
+    gpg_key = hookenv.config().get("gpg_public_key")
+    disable = hookenv.config().get("disable_encryption")
+    if not passphrase and not gpg_key and not disable:
+        hookenv.status_set('blocked', 'Must set either an encryption \
+        passphrase, GPG public key, or disable encryption')
+
+
+@when('config.changed')
+def validate_configs():
+    clear_flag('duplicity.configured')
+    valid = validate_backend() and \
+            validate_cron_frequency() and \
+            validate_encryption_method()
+
+    if valid:
+        set_flag('duplicity.configured')
+
+
+@when('duplicity.configured')
+def update_cron():
+    """
+    Finalizes the backup cron script when duplicity has been configured
+    successfully. The cron script will be a call to juju run-action do-backup
+    """
+    hookenv.status_set('active', '')
 
