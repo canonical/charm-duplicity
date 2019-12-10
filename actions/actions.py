6b394271fd5f6695b855b32cdbc3991a361a41bf
Zachary Zehring <zachary.zehring@canonical.com>
2019-12-10 16:40:48 -0500

    Refactor to main actions.py file and symlink action files.
    This mirrors many OpenStack charm action structures. The benefit here
    is we can avoid many of the same import calls. Most of the actual heavy
    lifting resides in the DuplicityHelper class. This allows this file to
    focus on logging/updating action results.
    
    - Add condition that duplicity unit must be active to run do-backup cmd
    - Provide output for action on failure/success
    - Pipe stderr to subprocess.STDOUT to grab error output

---
+++ actions/actions.py
@@ -0,0 +1,46 @@
+#!/usr/local/sbin/charm-env python3
+
+import os
+import sys
+from subprocess import CalledProcessError
+
+sys.path.append('lib')
+
+from charmhelpers.core import hookenv
+
+from lib.lib_duplicity import DuplicityHelper
+
+
+helper = DuplicityHelper()
+
+
+def do_backup(*args):
+    status, _ = hookenv.status_get()
+    if status != 'active':
+        hookenv.action_fail('Duplicity unit must be in ready state to execute do-backup command.')
+        return
+    output = helper.do_backup(logger=hookenv.log)
+    hookenv.action_set(dict(output=output.decode('utf-8')))
+
+
+ACTIONS = {
+    'do-backup': do_backup
+}
+
+
+def main(args):
+    action_name = os.path.basename(args[0])
+    action = ACTIONS.get(action_name)
+    if not action_name:
+        return 'Action "{}" is undefined'.format(action_name)
+    try:
+        action(args)
+    except CalledProcessError as e:
+        hookenv.action_fail('Command "{}" failed with return code "{}" and message:\n{}'.format(
+            e.cmd, e.returncode, e.output.decode('utf-8')))
+    except Exception as e:
+        hookenv.action_fail(str(e))
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
