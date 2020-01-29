b4210c35cada123072bfc0a12a643fe68e0c8cfd
Zachary Zehring <zachary.zehring@canonical.com>
2020-01-29 17:09:26 -0500

    Update and add unit tests with new validation logic.

--- tests/unit/test_reactive_duplicity.py
+++ tests/unit/test_reactive_duplicity.py
@@ -1,10 +1,11 @@
-from unittest.mock import patch, call, ANY, mock_open, MagicMock, Mock
+from unittest.mock import patch, call, ANY, mock_open, MagicMock, DEFAULT
 
 import pytest
 from croniter import CroniterBadCronError, CroniterBadDateError, CroniterNotAlphaError
 
-with patch('lib_duplicity.DuplicityHelper') as mock_duplicity_helper:
-    import duplicity
+with patch('lib_duplicity.DuplicityHelper') as helper_mock:
+    with patch('charmhelpers.core.hookenv') as hookenv_mock:
+        import duplicity
 
 
 @patch('duplicity.hookenv')
@@ -23,15 +24,13 @@ class TestValidateBackend:
     @pytest.mark.parametrize('backend,', ['ftp', 'file'])
     @patch('duplicity.set_flag')
     @patch('duplicity.clear_flag')
-    @patch('duplicity.helper')
-    @patch('duplicity.hookenv')
+    @patch('duplicity.config')
     def test_validate_backend_success_not_secured(
-            self, mock_hookenv, mock_helper, mock_clear_flag, mock_set_flag, backend):
-        mock_hookenv.config.return_value.get.side_effect = [backend, 'remote_url']
+            self, mock_config, mock_clear_flag, mock_set_flag, backend):
+        mock_config.get.return_value = backend
         duplicity.validate_backend()
-        mock_helper.update_known_host_file.assert_not_called()
-        mock_clear_flag.assert_not_called()
-        mock_set_flag.assert_called_with('duplicity.valid_backend')
+        mock_clear_flag.assert_called_with('duplicity.invalid_backend')
+        mock_set_flag.assert_not_called()
 
     @pytest.mark.parametrize('backend,remote_password,ssh_key', [
         ('scp', 'remote-pass', None),
@@ -43,90 +42,62 @@ class TestValidateBackend:
     ])
     @patch('duplicity.set_flag')
     @patch('duplicity.clear_flag')
-    @patch('duplicity.helper')
-    @patch('duplicity.hookenv')
+    @patch('duplicity.config')
     def test_validate_backend_success_secured(
-            self, mock_hookenv, mock_helper, mock_clear_flag, mock_set_flag, backend, remote_password, ssh_key):
+            self, mock_config, mock_clear_flag, mock_set_flag, backend, remote_password, ssh_key):
         known_host_key = 'host_key'
-        side_effects = [backend, known_host_key, remote_password]
-        if ssh_key:
-            side_effects.append(ssh_key)
-        side_effects.append('remote_url')
-        mock_hookenv.config.return_value.get.side_effect = side_effects
+        side_effects = [backend, known_host_key, remote_password, ssh_key]
+        mock_config.get.side_effect = side_effects
         duplicity.validate_backend()
-        mock_helper.update_known_host_file.assert_called_with(known_host_key)
-        mock_clear_flag.assert_not_called()
-        mock_set_flag.assert_called_with('duplicity.valid_backend')
+        mock_set_flag.assert_not_called()
+        mock_clear_flag.assert_called_with('duplicity.invalid_secure_backend_opts')
 
     @patch('duplicity.set_flag')
     @patch('duplicity.clear_flag')
-    @patch('duplicity.hookenv')
-    def test_invalid_backend_secured_no_host_key(self, mock_hookenv, mock_clear_flag, mock_set_flag):
+    @patch('duplicity.config')
+    def test_invalid_backend_secured_no_host_key(self, mock_config, mock_clear_flag, mock_set_flag):
         backend = 'scp'
-        mock_hookenv.config.return_value.get.side_effect = [backend, '']
+        mock_config.get.side_effect = [backend, None, None, None]
         duplicity.validate_backend()
-        mock_hookenv.status_set.assert_called_with(
-            'blocked', '{} backend requires known_host_key to be set.'.format(backend))
-        mock_clear_flag.assert_called_with('duplicity.valid_backend')
-        mock_set_flag.assert_not_called()
-
-    @pytest.mark.parametrize('remote_pass,ssh_key,remote_backup_url', [
-        ('', '', 'some_backup_url'),
-        ('some_pass', '', ''),
-    ])
-    @patch('duplicity.set_flag')
-    @patch('duplicity.clear_flag')
-    @patch('duplicity.helper')
-    @patch('duplicity.hookenv')
-    def test_invalid_backend_secured(
-            self, mock_hookenv, mock_helper, mock_clear_flag, mock_set_flag, remote_pass, ssh_key, remote_backup_url):
-        side_effects = ['scp', 'some_host', remote_pass, ssh_key, remote_backup_url]
-        mock_hookenv.config.return_value.get.side_effect = side_effects
-        duplicity.validate_backend()
-        mock_helper.update_known_host_file.assert_called()
-        mock_hookenv.status_set.assert_called_with('blocked', ANY)
-        mock_clear_flag.assert_called_with('duplicity.valid_backend')
-        mock_set_flag.assert_not_called()
+        mock_set_flag.assert_called_with('duplicity.invalid_secure_backend_opts')
+        mock_clear_flag.assert_called_with('duplicity.invalid_backend')
 
     @patch('duplicity.set_flag')
     @patch('duplicity.clear_flag')
-    @patch('duplicity.hookenv')
-    def test_validate_backend_success_s3(self, mock_hookenv, mock_clear_flag, mock_set_flag):
+    @patch('duplicity.config')
+    def test_validate_backend_success_s3(self, mock_config, mock_clear_flag, mock_set_flag):
         backend = 's3'
-        mock_hookenv.config.return_value.get.side_effect = [
-            backend, 'aws_id', 'aws_secret', 'some_remote_url'
-        ]
+        mock_config.get.side_effect = [backend, 'aws_id', 'aws_secret']
+        expected_calls = [call('duplicity.invalid_backend'), call('duplicity.invalid_aws_creds')]
         duplicity.validate_backend()
-        mock_clear_flag.assert_not_called()
-        mock_set_flag.assert_called_with('duplicity.valid_backend')
+        mock_clear_flag.assert_has_calls(calls=expected_calls)
+        mock_set_flag.assert_not_called()
 
     @pytest.mark.parametrize('key_id,secret_key', [
         ('some_id', ''),
         ('', 'some_key'),
+        ('', ''),
     ])
     @patch('duplicity.set_flag')
     @patch('duplicity.clear_flag')
-    @patch('duplicity.hookenv')
-    def test_invalid_backend_s3(self, mock_hookenv, mock_clear_flag, mock_set_flag, key_id, secret_key):
+    @patch('duplicity.config')
+    def test_invalid_backend_s3(self, mock_config, mock_clear_flag, mock_set_flag, key_id, secret_key):
         backend = 's3'
         side_effects = [backend, key_id, secret_key]
-        mock_hookenv.config.return_value.get.side_effect = side_effects
+        mock_config.get.side_effect = side_effects
         duplicity.validate_backend()
-        mock_hookenv.status_set.assert_called_with(
-            'blocked', 'S3 backups require "aws_access_key_id" and "aws_secret_access_key" to be set')
-        mock_clear_flag.assert_called_with('duplicity.valid_backend')
-        mock_set_flag.assert_not_called()
+        mock_clear_flag.assert_called_with('duplicity.invalid_backend')
+        mock_set_flag.assert_called_with('duplicity.invalid_aws_creds')
 
     @patch('duplicity.set_flag')
     @patch('duplicity.clear_flag')
-    @patch('duplicity.hookenv')
-    def test_invalid_backend_bad_backend(self, mock_hookenv, mock_clear_flag, mock_set_flag):
+    @patch('duplicity.config')
+    def test_invalid_backend_bad_backend(self, mock_config, mock_clear_flag, mock_set_flag):
         backend = 'bad_backend'
-        mock_hookenv.config.return_value.get.return_value = backend
+        mock_config.get.return_value = backend
         duplicity.validate_backend()
-        mock_hookenv.status_set.assert_called_with('blocked', 'Unrecognized backend "{}"'.format(backend))
-        mock_clear_flag.assert_called_with('duplicity.valid_backend')
-        mock_set_flag.assert_not_called()
+        mock_set_flag.assert_called_with('duplicity.invalid_backend')
+        mock_clear_flag.assert_not_called()
 
 
 @pytest.mark.parametrize('backup_dir,path_exists', [
@@ -134,10 +105,10 @@ class TestValidateBackend:
     ('my dir', True),
     ('', True),
 ])
-@patch('duplicity.hookenv')
+@patch('duplicity.config')
 @patch('duplicity.os')
-def test_create_aux_backup_directory(mock_os, mock_hookenv, backup_dir, path_exists):
-    mock_hookenv.config.return_value.get.return_value = backup_dir
+def test_create_aux_backup_directory(mock_os, mock_config, backup_dir, path_exists):
+    mock_config.get.return_value = backup_dir
     mock_os.path.exists.return_value = path_exists
     duplicity.create_aux_backup_directory()
     if path_exists or not backup_dir:
@@ -154,42 +125,82 @@ class TestValidateCronFrequency:
         ('monthly', True),
         ('manual', False),
     ])
-    @patch('duplicity.hookenv')
+    @patch('duplicity.clear_flag')
     @patch('duplicity.set_flag')
-    def test_valid_cron_frequency(self, mock_set_flag, mock_hookenv, frequency, create_cron):
+    @patch('duplicity.config')
+    def test_valid_cron_frequency(self, mock_config, mock_set_flag, mock_clear_flag, frequency, create_cron):
         set_flag_arg = 'duplicity.create_backup_cron' if create_cron else 'duplicity.remove_backup_cron'
-        calls = [call(set_flag_arg), call('duplicity.valid_backup_frequency')]
-        mock_hookenv.config.return_value.get.return_value = frequency
+        calls = [call(set_flag_arg)]
+        mock_config.get.return_value = frequency
         duplicity.validate_cron_frequency()
         mock_set_flag.assert_has_calls(calls)
+        mock_clear_flag.assert_called_with('duplicity.invalid_backup_frequency')
 
     @patch('duplicity.croniter')
-    @patch('duplicity.hookenv')
+    @patch('duplicity.clear_flag')
     @patch('duplicity.set_flag')
-    def test_valid_cron_string(self, mock_set_flag, mock_hookenv, mock_croniter):
+    @patch('duplicity.config')
+    def test_valid_cron_string(self, mock_config, mock_set_flag, mock_clear_flag, mock_croniter):
         valid_cron_string = '* * * * *'
-        mock_hookenv.config.return_value.get.return_value = valid_cron_string
-        calls = [call('duplicity.create_backup_cron'), call('duplicity.valid_backup_frequency')]
+        mock_config.get.return_value = valid_cron_string
+        calls = [call('duplicity.create_backup_cron')]
         duplicity.validate_cron_frequency()
         mock_croniter.croniter.assert_called_with(valid_cron_string)
         mock_set_flag.assert_has_calls(calls)
+        mock_clear_flag.assert_called_with('duplicity.invalid_backup_frequency')
 
     @pytest.mark.parametrize('raise_error', [CroniterBadDateError, CroniterNotAlphaError, CroniterBadCronError])
-    @patch('duplicity.clear_flag')
     @patch('duplicity.croniter.croniter')
-    @patch('duplicity.hookenv')
+    @patch('duplicity.clear_flag')
     @patch('duplicity.set_flag')
-    def test_invalid_cron_string(self, mock_set_flag, mock_hookenv, mock_croniter, mock_clear_flag, raise_error):
+    @patch('duplicity.config')
+    def test_invalid_cron_string(self, mock_config, mock_set_flag, mock_clear_flag, mock_croniter, raise_error):
         invalid_cron_string = '* * * *'
-        mock_hookenv.config.return_value.get.return_value = invalid_cron_string
+        mock_config.get.return_value = invalid_cron_string
         mock_croniter.side_effect = raise_error
-        clear_flag_calls = [call('duplicity.valid_backup_frequency'), call('duplicity.create_backup_cron')]
+        clear_flag_calls = [call('duplicity.create_backup_cron')]
         duplicity.validate_cron_frequency()
-        args, _ = mock_hookenv.status_set.call_args
-        assert args[0] == 'blocked'
-        assert invalid_cron_string in args[1]
         mock_clear_flag.assert_has_calls(clear_flag_calls)
-        mock_set_flag.assert_not_called()
+        mock_set_flag.assert_called_with('duplicity.invalid_backup_frequency')
+
+
+class TestUpdateKnownHostKey:
+    @patch('duplicity.helper')
+    @patch('duplicity.hookenv')
+    @patch('duplicity.config')
+    def test_update_known_host_key_set(self, mock_config, mock_hookenv, mock_helper):
+        host_key = 'somekey'
+        mock_config.get.return_value = host_key
+        duplicity.update_known_host_key()
+        mock_hookenv.status_set.assert_called_with(
+            workload_state='maintenance',
+            message='Updating known_host_key'
+        )
+        mock_helper.update_known_host_file.assert_called_with(host_key)
+
+    @patch('duplicity.helper')
+    @patch('duplicity.config')
+    def test_update_known_host_key_unset(self, mock_config, mock_helper):
+        mock_config.get.return_value = ''
+        duplicity.update_known_host_key()
+        mock_helper.update_known_host_file.assert_not_called()
+
+
+@pytest.mark.parametrize('remote_backup_url,is_valid', [
+    ('', False),
+    ('some.url', True)
+])
+@patch('duplicity.clear_flag')
+@patch('duplicity.set_flag')
+@patch('duplicity.config')
+def test_check_remote_backup_url(mock_config, mock_set_flag, mock_clear_flag, remote_backup_url, is_valid):
+    mock_config.get.return_value = remote_backup_url
+    duplicity.check_remote_backup_url()
+    if is_valid:
+        mock_clear_flag.assert_called_with('duplicity.invalid_backend')
+    else:
+        mock_set_flag.assert_called_with('duplicity.invalid_backend')
+
 
 
 @pytest.mark.parametrize('encryption_passphrase,gpg_key,disable_encryption,valid', [
@@ -198,31 +209,65 @@ class TestValidateCronFrequency:
     (None, None, True, True),
     (None, None, False, False),
 ])
-@patch('duplicity.hookenv')
 @patch('duplicity.set_flag')
 @patch('duplicity.clear_flag')
+@patch('duplicity.config')
 def test_validate_encryption_method(
-        mock_clear_flag, mock_set_flag, mock_hookenv, encryption_passphrase, gpg_key, disable_encryption, valid):
-    mock_hookenv.config.return_value.get.side_effect = [encryption_passphrase, gpg_key, disable_encryption]
+        mock_config, mock_clear_flag, mock_set_flag, encryption_passphrase, gpg_key, disable_encryption, valid):
+    mock_config.get.side_effect = [encryption_passphrase, gpg_key, disable_encryption]
     duplicity.validate_encryption_method()
     if valid:
-        mock_set_flag.assert_called_with('duplicity.valid_encryption_method')
+        mock_clear_flag.assert_called_with('duplicity.invalid_encryption_method')
     else:
-        mock_hookenv.status_set('blocked', ANY)
-        mock_clear_flag.assert_called_with('duplicity.valid_encryption_method')
+        mock_set_flag.assert_called_with('duplicity.invalid_encryption_method')
 
 
-@patch('duplicity.hookenv')
-def test_app_ready(mock_hookenv):
-    duplicity.app_ready()
-    mock_hookenv.status_set.assert_called_with('active', 'Ready')
+class TestCheckStatus:
+    @patch('duplicity.hookenv')
+    def test_check_status(self, mock_hookenv):
+        duplicity.check_status()
+        mock_hookenv.atexit.assert_called_with(duplicity.assess_status)
+
+    @pytest.mark.parametrize('invalid_flag,message,index', [
+        ('duplicity.invalid_remote_backup_url', 'Backup path is required. Set config for "remote_backup_url"', 0),
+        ('duplicity.invalid_backend', 'Unrecognized backend "{}"', 1),
+        ('duplicity.invalid_aws_creds',
+         'S3 backups require "aws_access_key_id" and "aws_secret_access_key" to be set', 2),
+        ('duplicity.invalid_secure_backend_opts',
+         '{} backend requires known_host_key and either "remote_password" or "private_ssh_key" to be set', 3),
+        ('duplicity.invalid_encryption_method',
+         'Must set either an encryption passphrase, GPG public key, or disable encryption', 4),
+        ('duplicity.invalid_private_ssh_key', 'Invalid private_ssh_key. ensure that key is base64 encoded', 5),
+        ('duplicity.invalid_backup_frequency', 'Invalid value "{}" for "backup_frequency"', 6),
+    ])
+    @patch('duplicity.is_flag_set')
+    @patch('duplicity.hookenv')
+    @patch('duplicity.config')
+    def test_blocked_assess_status(self, mock_config, mock_hookenv, mock_is_flag_set, invalid_flag, message, index):
+        side_effects = [False for _ in range(index)] + [True]
+        mock_is_flag_set.side_effect = side_effects
+        some_val = 'some_val'
+        mock_config.get.return_value = some_val
+        duplicity.assess_status()
+        mock_hookenv.status_set.assert_called_with(
+            workload_state='blocked',
+            message=message.format(some_val)
+        )
+
+    @patch('duplicity.is_flag_set')
+    @patch('duplicity.hookenv')
+    def test_good_assess_status(self, mock_hookenv, mock_is_flag_set):
+        mock_is_flag_set.return_value = False
+        duplicity.assess_status()
+        mock_hookenv.status_set.assert_has_calls(calls=[call('active', 'Ready')])
 
 
 @patch('duplicity.hookenv')
 @patch('duplicity.clear_flag')
 @patch('duplicity.helper')
 def test_create_backup_cron(mock_helper, mock_clear_flag, mock_hookenv):
-    hookenv_calls = [call('active', 'Rendering duplicity crontab'), call('active', 'Ready')]
+    hookenv_calls = [call('maintenance', 'Rendering duplicity crontab'),
+                     call('active', 'Rendered duplicity crontab')]
     duplicity.create_backup_cron()
     mock_hookenv.status_set.assert_has_calls(hookenv_calls)
     mock_helper.setup_backup_cron.assert_called_once()
@@ -242,10 +287,10 @@ class TestUpdatePrivateSshKey:
     @patch('duplicity.clear_flag')
     @patch('duplicity.base64')
     @patch('duplicity.hookenv')
-    def test_update_key_success(self, mock_hookenv, mock_base64, mock_clear_flag):
+    def test_update_key_success(self, mock_config, mock_base64, mock_clear_flag):
         private_key = 'a_key'
         decoded_key = 'a_decoded_key'
-        mock_hookenv.config.return_value.get.return_value = private_key
+        mock_config.get.return_value = private_key
         mock_base64.b64decode.return_value.decode.return_value = decoded_key
         with patch('duplicity.open', mock_open()) as m_open:
             duplicity.update_private_ssh_key()
@@ -258,9 +303,9 @@ class TestUpdatePrivateSshKey:
     @pytest.mark.parametrize('path_exists', [True, False])
     @patch('duplicity.clear_flag')
     @patch('duplicity.os')
-    @patch('duplicity.hookenv')
-    def test_update_key_no_key(self, mock_hookenv, mock_os, mock_clear_flag, path_exists):
-        mock_hookenv.config.return_value.get.return_value = ''
+    @patch('duplicity.config')
+    def test_update_key_no_key(self, mock_config, mock_os, mock_clear_flag, path_exists):
+        mock_config.get.return_value = ''
         mock_os.path.exists.return_value = path_exists
         duplicity.update_private_ssh_key()
         assert mock_os.remove.called == path_exists
@@ -268,12 +313,10 @@ class TestUpdatePrivateSshKey:
 
     @patch('duplicity.clear_flag')
     @patch('duplicity.set_flag')
-    @patch('duplicity.hookenv')
-    def test_update_key_fail(self, mock_hookenv, mock_set_flag, mock_clear_flag):
-        mock_hookenv.config.return_value.get.return_value = 'invalid_key'
+    @patch('duplicity.config')
+    def test_update_key_fail(self, mock_config, mock_set_flag, mock_clear_flag):
+        mock_config.get.return_value = 'invalid_key'
         duplicity.update_private_ssh_key()
-        mock_hookenv.status_set.assert_called_with(
-            workload_state='blocked', message='invalid private_ssh_key. ensure that key is base64 encoded')
         mock_set_flag.assert_called_with('duplicity.invalid_private_ssh_key')
         mock_clear_flag.assert_not_called()
 
