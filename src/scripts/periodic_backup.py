2e9c196fde984199541368a45ca493705469b3ec
peppepetra86 <giuseppe.petralia@canonical.com>
2020-08-12 17:37:52 +0200

    Blackened repository to 88 lines

--- src/scripts/periodic_backup.py
+++ src/scripts/periodic_backup.py
@@ -6,7 +6,7 @@ import os
 import subprocess
 import sys
 
-sys.path.append('lib')
+sys.path.append("lib")
 
 from charmhelpers.core import hookenv
 
@@ -17,41 +17,53 @@ from lib import lib_duplicity
 from pidfile import PidFile
 
 
-pidfile = '/var/run/periodic_backup.pid'
-error_file = '/var/run/periodic_backup.error'
+pidfile = "/var/run/periodic_backup.pid"
+error_file = "/var/run/periodic_backup.error"
 
 
 def write_error_file(message):
     """Write error log if backup failed."""
-    with open(error_file, 'w') as f:
+    with open(error_file, "w") as f:
         f.write(message)
 
 
 def main():
     """Periodically backup with juju run-action do-backup."""
     try:
-        hookenv.log('Performing backup.')
+        hookenv.log("Performing backup.")
         output = lib_duplicity.DuplicityHelper().do_backup()
-        hookenv.log('Periodic backup complete with following output:\n{}'.format(output.decode('utf-8')))
+        hookenv.log(
+            "Periodic backup complete with following output:\n{}".format(
+                output.decode("utf-8")
+            )
+        )
     except subprocess.CalledProcessError as e:
-        err_msg = 'Periodic backup failed. Command "{}" failed with return code "{}" and error output:\n{}'.format(
-            e.cmd, e.returncode, e.output.decode('utf-8'))
+        err_msg = (
+            'Periodic backup failed. Command "{}" failed with return code "{}" '
+            "and error output:\n{}".format(
+                e.cmd, e.returncode, e.output.decode("utf-8")
+            )
+        )
+
         hookenv.log(err_msg, level=hookenv.ERROR)
         write_error_file(err_msg)
     except Exception as e:
-        err_msg = 'Periodic backup failed: {}'.format(str(e))
+        err_msg = "Periodic backup failed: {}".format(str(e))
         hookenv.log(err_msg, level=hookenv.ERROR)
         write_error_file(err_msg)
     else:
-        clear_flag('duplicity.failed_backup')
+        clear_flag("duplicity.failed_backup")
         if os.path.exists(error_file):
             os.remove(error_file)
 
 
 if __name__ == "__main__":
     status, workload_msg = hookenv.status_get()
-    if status != 'active':
-        hookenv.log('Duplicity unit must be in ready state to execute backup command.', level=hookenv.WARNING)
+    if status != "active":
+        hookenv.log(
+            "Duplicity unit must be in ready state to execute backup command.",
+            level=hookenv.WARNING,
+        )
         sys.exit()
     with PidFile(pidfile):
         main()
--- src/tests/functional/tests/utils.py
+++ src/tests/functional/tests/utils.py
@@ -10,35 +10,43 @@ def get_app_config(app_name):
 
 def get_workload_application_status_checker(application_name, target_status):
     """ Returns a function for checking the status of all units of an application. """
+
     async def checker():
         units = await zaza.model.async_get_units(application_name)
-        unit_statuses_blocked = [unit.workload_status == target_status for unit in units]
+        unit_statuses_blocked = [
+            unit.workload_status == target_status for unit in units
+        ]
         return all(unit_statuses_blocked)
+
     return checker
 
 
 def config_restore(*applications):
     def config_restore_wrap(f):
-        AppConfigPair = namedtuple('AppConfigPair', ['app_name', 'config'])
+        AppConfigPair = namedtuple("AppConfigPair", ["app_name", "config"])
 
         @wraps(f)
         def wrapped_f(*args):
-            original_configs = [AppConfigPair(app, get_app_config(app)) for app in applications]
+            original_configs = [
+                AppConfigPair(app, get_app_config(app)) for app in applications
+            ]
             try:
                 f(*args)
             finally:
                 for app_config_pair in original_configs:
-                    zaza.model.set_application_config(app_config_pair.app_name, app_config_pair.config)
+                    zaza.model.set_application_config(
+                        app_config_pair.app_name, app_config_pair.config
+                    )
                 zaza.model.block_until_all_units_idle(timeout=60)
+
         return wrapped_f
+
     return config_restore_wrap
 
 
 def set_config_and_wait(application_name, config, model_name=None):
     zaza.model.set_application_config(
-        application_name=application_name,
-        configuration=config,
-        model_name=model_name
+        application_name=application_name, configuration=config, model_name=model_name
     )
     zaza.model.block_until_all_units_idle()
 
